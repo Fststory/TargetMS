ea3852c6f4c5f7e97a8a759804bc2e44f432423e
cob1887 <maincore449@hotmail.com>
2024-09-02 15:12:47 +0900

    f

--- Assets/PJH/P_Scripts/Connect_Manager.cs
+++ Assets/PJH/P_Scripts/Connect_Manager.cs
@@ -6,11 +6,11 @@ using JetBrains.Annotations;
 using System;
 using Photon.Realtime;
 
-public class Connect_Manager : MonoBehaviourPunCallbacks
+public class Connect_Manager : MonoBehaviourPunCallbacks   // 해당 씬에 들어오자마자 마스터-채널-로비-방생성까지 한번에 되게 설정, 경우에 따라 함수를 스타트에 붙여넣기
 {
     void Start()
     {
-        // 해당 씬에 들어오자마자 마스터-채널-로비-방생성까지 한번에 되게 설정, 경우에 따라 함수를 스타트에 붙여넣기
+        
         PhotonNetwork.ConnectUsingSettings();
     }
 
@@ -19,54 +19,67 @@ public class Connect_Manager : MonoBehaviourPunCallbacks
         
     }
 
-    // 마스터 서버 접속시 
-    public override void OnConnected()
+    // 마스터 서버 접속시 호출되는 함수
+    public override void OnConnectedToMaster()
     {
         Debug.Log("마스터 서버에 입장했습니다.");
         base.OnConnectedToMaster();
 
     }
 
-    // 로비 접속시
+    // 로비 접속시 호출되는 함수
     public override void OnJoinedLobby()
     {
-        Debug.Log("로비에 접속했습니다");
         base.OnJoinedLobby();
+        Debug.Log("로비에 접속했습니다");
+        JoinOrCreateRoom();
     }
 
-    // 로비 접속시 방을 찾고 없으면 만들기
     public void JoinOrCreateRoom()
-    {
+    {       
+        // 방이 없다면 방 옵션으로
         RoomOptions roomOptions = new RoomOptions();
-
-
-        // 방을 찾기
-        PhotonNetwork.JoinOrCreateRoom("Room1", roomOptions, TypedLobby.Default);
-        PhotonNetwork.JoinOrCreateRoom("Room2", roomOptions, TypedLobby.Default);
-        PhotonNetwork.JoinOrCreateRoom("Room3", roomOptions, TypedLobby.Default);
-
-        // 방이 없다면 아래의 정보로 생성
-        roomOptions.MaxPlayers = 5;
+        roomOptions.MaxPlayers = 2;
         roomOptions.IsVisible = true;
         roomOptions.IsOpen = true;
-                       
-    }
 
-    // 방이 있을때
-    public override void OnJoinedRoom()
-    {
-        Debug.Log("방에 입장합니다.");
-        base.OnJoinedRoom();
-
-        PhotonNetwork.LoadLevel("GameScene");
+        PhotonNetwork JoinOrCreateRoom("TargetMs_Room_1", roomOptions, TypeLobby.Defalut);
     }
    
-    // 방이 없어서 생성됬을때
-    public override void OnCreatedRoom()
-    {
-        Debug.Log("방을 만들었습니다.");
-        base.OnCreatedRoom();
-    }
+
+    //// 로비 접속시 방을 찾고 없으면 만들기
+    //public void JoinOrCreateRoom()
+    //{
+    //    RoomOptions roomOptions = new RoomOptions();
+
+
+    //    // 방을 찾기
+    //    PhotonNetwork.JoinOrCreateRoom("Room1", roomOptions, TypedLobby.Default);
+    //    PhotonNetwork.JoinOrCreateRoom("Room2", roomOptions, TypedLobby.Default);
+    //    PhotonNetwork.JoinOrCreateRoom("Room3", roomOptions, TypedLobby.Default);
+
+    //    // 방이 없다면 아래의 정보로 생성
+    //    roomOptions.MaxPlayers = 5;
+    //    roomOptions.IsVisible = true;
+    //    roomOptions.IsOpen = true;
+
+    //}
+
+    //// 방이 있을때
+    //public override void OnJoinedRoom()
+    //{
+    //    Debug.Log("방에 입장합니다.");
+    //    base.OnJoinedRoom();
+
+    //    PhotonNetwork.LoadLevel("GameScene");
+    //}
+
+    //// 방이 없어서 생성됬을때
+    //public override void OnCreatedRoom()
+    //{
+    //    Debug.Log("방을 만들었습니다.");
+    //    base.OnCreatedRoom();
+    //}
 
 
 }
