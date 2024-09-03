ae8e21d2e11d867eb449664c79439de566973d5e
cob1887 <maincore449@hotmail.com>
2024-09-03 17:25:52 +0900

    LobbyScene Test

---
+++ Assets/PJH/P_Scripts/Alert_Event_Manager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Alert_Event_Manager : MonoBehaviour // 돌발이벤트 발생 스크립트
+{
+    public GameObject AlertPanel;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Player 이름의 게임 오브젝트가 닿으면
+        if(other.name.Contains("Player"))
+        {
+
+        }
+    }
+
+}
