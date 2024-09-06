8992e780ae945532f30b12fdad3ab350470d1a4b
Cowshapetea <kimdon000@gmail.com>
2024-09-06 17:37:14 +0900

    마이크 할당 + 플레이어 영역 들어가면 색깔 바뀌는거 성공

--- Assets/KDH/Scripts/Area.cs
+++ Assets/KDH/Scripts/Area.cs
@@ -27,10 +27,33 @@ public class Area : MonoBehaviour
             // 영역에 닿은 오브젝트의 Renderer를 가져온다
             Renderer objectRenderer = GetComponent<Renderer>();
 
-            // Renderer가 있다면 색상을 초록색으로 변경
+            // Renderer가 있다면 색상을 노랑 변경
             if (objectRenderer != null)
             {
                 objectRenderer.material.color = Color.yellow;
+                //objectRenderer.material.color = new Color(255, 255, 0, 1f);
+
+                //Image image = GetComponent<Image>();
+                //image.color = new Color(image.color.r, image.color.g, image.color.b, 1f);
+            }
+
+        }
+    }
+    // 플레이어가 영역에서 나갔을 때 호출되는 함수
+    private void OnTriggerExit(Collider other)
+    {
+        // "Player" 태그가 붙은 오브젝트가 나갔을 때만
+        if (other.CompareTag("Player"))
+        {
+            // 영역에 닿은 오브젝트의 Renderer를 가져온다
+            Renderer objectRenderer = GetComponent<Renderer>();
+
+            if (objectRenderer != null)
+            {
+                // 색상을 원래 색상으로 되돌리기
+                objectRenderer.material.color = new Color(0, 0.5f, 0, 1); // 소숫점 단위였음 ㅋㅋ
+                //objectRenderer.material.color = new Color(255, 255, 0, 1f); // 이거는 안됨ㅋㅋ
+                //objectRenderer.material.color = Color.;
             }
         }
     }
