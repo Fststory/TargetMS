de4e715a821721918556e81022d2bb0d27d42e6a
Fststory <fststory@naver.com>
2024-09-10 15:41:21 +0900

    itextsharp

---
+++ Assets/KYH/Scripts/PDF_itextsharp/PDFGenerator.cs
@@ -0,0 +1,16 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
+
+public class PDFGenerator
+{
+    public void CreatePDF(string filePath)
+    {
+        Document document = new Document();
+        PdfWriter.GetInstance(document, new FileStream(filePath, FileMode.Create));
+        document.Open();
+        document.Add(new Paragraph("Hello, iTextSharp!"));
+        document.Add(new Paragraph("My name is Kim Young Ho!"));
+        document.Close();
+    }
+}
