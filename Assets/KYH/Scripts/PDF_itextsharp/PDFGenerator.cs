c44b5ac7ccc3b2b9a4400f0157f1f376509c8816
Fststory <fststory@naver.com>
2024-09-11 14:57:58 +0900

    itextsharp 적용 완료

--- Assets/KYH/Scripts/PDF_itextsharp/PDFGenerator.cs
+++ Assets/KYH/Scripts/PDF_itextsharp/PDFGenerator.cs
@@ -1,9 +1,15 @@
 ﻿using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
+using UnityEngine;
 
-public class PDFGenerator
+public class PDFGenerator : MonoBehaviour
 {
+    private void Start()
+    {
+        CreatePDF(Application.dataPath + "/test.pdf");
+    }
+
     public void CreatePDF(string filePath)
     {
         Document document = new Document();
@@ -11,6 +17,9 @@ public class PDFGenerator
         document.Open();
         document.Add(new Paragraph("Hello, iTextSharp!"));
         document.Add(new Paragraph("My name is Kim Young Ho!"));
+        document.Add(new Paragraph("My name is Kim Dong Hwi!"));
+        document.Add(new Paragraph("My name is Park Jeong Hyun!"));
         document.Close();
+        print("PDF 생성 완료!");
     }
 }
