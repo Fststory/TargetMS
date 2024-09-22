ac977a4fc6beb4e8d1d634ff40b941a47ef498e0
Fststory <fststory@naver.com>
2024-09-22 13:41:41 +0900

    240922 안 쓰는 것들 정리

--- Assets/KYH/Scripts/ProposalMgr.cs
+++ Assets/KYH/Scripts/ProposalMgr.cs
@@ -27,6 +27,14 @@ public class ProposalMgr : MonoBehaviour
 {
     // 기획서 요청 통신 스크립트
 
+    // 다른 스크립트에서 접근 용이
+    /// <summary>
+    ///  예시) 체크리스트 작성 씬에서 사용할 때 "3/4C분석" 버튼을 누르면(OnClick()) 발동되는 함수
+    ///  void Btn1()
+    ///  {
+    ///      ProposalMgr.instance.proposal.analysis_type = "3/4C분석";
+    ///  }
+    /// </summary>
     public static ProposalMgr instance;
 
     // Post 서버 URL
@@ -35,6 +43,10 @@ public class ProposalMgr : MonoBehaviour
     // 출력받을 TMP_Text
     public TMP_Text text_response;
 
+    // 기획서 프롬프트
+    public Proposal proposal = new Proposal();
+
+    // 싱글턴 패턴
     private void Awake()
     {
         if (instance == null)
@@ -43,16 +55,8 @@ public class ProposalMgr : MonoBehaviour
         }
     }
 
-    void Start()
-    {
-        PostJson();
-    }
-
-    void Update()
-    {
-        
-    }
-
+    // 프롬프트(체크 리스트) 제출 기능 함수
+    // 작성 버튼에 연결시키면 됨!!
     public void PostJson()
     {
         StartCoroutine(PostJsonRequest(url));
@@ -60,18 +64,17 @@ public class ProposalMgr : MonoBehaviour
 
     IEnumerator PostJsonRequest(string url)
     {
-        // 사용자의 입력 정보(선택한 키워드)를 Json 데이터로 변환하기
-        Proposal proposal = new Proposal();
-        proposal.analysis_type = "3/4C분석";
-        proposal.audience_type = "사내대상";
-        proposal.subject = "새로운 세대의 사용자 경험을 반영한 모바일 애플리케이션을 통해 시장 점유율을 높이고, 경쟁사 대비 차별화된 기능을 제공하기 위해";
-        proposal.project_type = "어플리케이션";
-        proposal.step = "컨셉제안";
-        proposal.plan = "2월~5월";
-        proposal.target = "20대 중반에서 30대 초반의 IT에 익숙한 직장인";
-        proposal.purpose = "사용자들이 더 쉽게 상품을 검색하고 구매할 수 있는 통합 쇼핑 플랫폼을 제공하여 사용자 편의성을 극대화하고, 기업 매출 성장을 목표로 함";
-        proposal.worker = "";
-        proposal.budget = "1억 2000만원";
+        // 사용자의 입력 정보(선택한 키워드)를 Json 데이터로 변환하기 (예시)
+        //proposal.analysis_type = "3/4C분석";
+        //proposal.audience_type = "사내대상";
+        //proposal.subject = "새로운 세대의 사용자 경험을 반영한 모바일 애플리케이션을 통해 시장 점유율을 높이고, 경쟁사 대비 차별화된 기능을 제공하기 위해";
+        //proposal.project_type = "어플리케이션";
+        //proposal.step = "컨셉제안";
+        //proposal.plan = "2월~5월";
+        //proposal.target = "20대 중반에서 30대 초반의 IT에 익숙한 직장인";
+        //proposal.purpose = "사용자들이 더 쉽게 상품을 검색하고 구매할 수 있는 통합 쇼핑 플랫폼을 제공하여 사용자 편의성을 극대화하고, 기업 매출 성장을 목표로 함";
+        //proposal.worker = "";
+        //proposal.budget = "1억 2000만원";
 
         string userJsonData = JsonUtility.ToJson(proposal, true);
         byte[] jsonBins = Encoding.UTF8.GetBytes(userJsonData);
