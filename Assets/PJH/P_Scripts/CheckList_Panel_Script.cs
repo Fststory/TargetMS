759af89eebc763d0a508cd48f01756957562b877
cob1887 <maincore449@hotmail.com>
2024-09-25 13:46:48 +0900

    캔버스 수정

--- Assets/PJH/P_Scripts/CheckList_Panel_Script.cs
+++ Assets/PJH/P_Scripts/CheckList_Panel_Script.cs
@@ -14,6 +14,8 @@ public class CheckList_Panel_Script : MonoBehaviour
     public GameObject button5;
     public GameObject button6;
 
+    public InputField inputfield;
+
 
     void Start()
     {
@@ -26,70 +28,7 @@ public class CheckList_Panel_Script : MonoBehaviour
     }
 
     // 정답 클릭할때
-    public void OnClick1(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.analysis_type = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
-    public void OnClick2(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.audience_type = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
-    public void OnClick3(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.subject = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
-    public void OnClick4(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
+    public void OnClick1(Button selectedBtn) // 유형 답변
     {
         // 버튼의 자식에서 TMP_Text를 가져온다.
         TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
@@ -110,7 +49,7 @@ public class CheckList_Panel_Script : MonoBehaviour
             Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
         }
     }
-    public void OnClick5(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
+    public void OnClick2(Button selectedBtn) // 읽는 이
     {
         // 버튼의 자식에서 TMP_Text를 가져온다.
         TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
@@ -122,7 +61,7 @@ public class CheckList_Panel_Script : MonoBehaviour
             //Debug.Log(text);
 
             // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.step = text;
+            ProposalMgr.instance.proposal.audience_type = text;
 
             // 그리고 진행도에 정답 이미지 추가할것
         }
@@ -131,28 +70,7 @@ public class CheckList_Panel_Script : MonoBehaviour
             Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
         }
     }
-    public void OnClick6(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.plan = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
-    public void OnClick7(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
+    public void OnClick3(Button selectedBtn) // 타겟
     {
         // 버튼의 자식에서 TMP_Text를 가져온다.
         TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
@@ -173,75 +91,34 @@ public class CheckList_Panel_Script : MonoBehaviour
             Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
         }
     }
-    public void OnClick8(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.purpose = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
-    public void OnClick9(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.worker = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
-    public void OnClick10(Button selectedBtn) // 이텍스트는 캔버스 3과 json 문서로 보낸다
-    {
-        // 버튼의 자식에서 TMP_Text를 가져온다.
-        TMP_Text btnText = selectedBtn.GetComponentInChildren<TMP_Text>();
-
-        if (btnText != null)
-        {
-            // TMP_Text의 텍스트를 가져온다.
-            string text = btnText.text;
-            //Debug.Log(text);
-
-            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
-            ProposalMgr.instance.proposal.budget = text;
-
-            // 그리고 진행도에 정답 이미지 추가할것
-        }
-        else
-        {
-            Debug.LogWarning("TMP_Text를 찾을 수 없습니다.");
-        }
-    }
+    
 
 
     // 오답을 클릭했을때 
     public void OnClikcedWrong()
     {
         // 진행도 오답 표시 추가
+    }
+
+    public void Input1() // 입력칸 1
+    {
+        // input 필드 안의 tmptext 가져온다
+        TMP_Text inputtext = inputfield.GetComponent<TMP_Text>();
+
+        if(inputtext != null)
+        {
+            // 안에 적힐 글을 text 변수 선언을 하고
+            string text = inputtext.text;
+
+            // 해당 버튼의 inputtext를 proposalMGR로 보낸다.
+            ProposalMgr.instance.proposal.target = text;
+        }
+    }
+
+    public void Input2() // 입력칸 2
+    {
+
+        //inputfield
     }
 }
 
