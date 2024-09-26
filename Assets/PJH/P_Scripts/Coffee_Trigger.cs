b49ee1635dd4d31e86628c72c10cf948af37d4e0
cob1887 <maincore449@hotmail.com>
2024-09-26 16:58:34 +0900

    멀티용

--- Assets/PJH/P_Scripts/Coffee_Trigger.cs
+++ Assets/PJH/P_Scripts/Coffee_Trigger.cs
@@ -5,42 +5,42 @@ using UnityEngine;
 
 public class Coffee_Trigger : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
-    {
-        // player move 컴포넌트를 가진 other 가 충돌하면
-        if (other.gameObject.CompareTag("Player"))
-        {
-            // 캐릭터는 동작, 회전을 멈추고
-
-            // 현재 캔버스를 true로
-            UI_Manager.instance.OnCoffee();
-        }
-    }
-
-
-    //////////////////////// 포톤 멀티 플레이용 
     //void OnTriggerEnter(Collider other)
     //{
     //    // player move 컴포넌트를 가진 other 가 충돌하면
-    //    if (other.CompareTag("Player"))
+    //    if (other.gameObject.CompareTag("Player"))
     //    {
-    //        PhotonView photonview = other.GetComponent<PhotonView>();
-
-    //        if(photonview != null && photonview.IsMine)
-    //        {
-    //            photonview.RPC("ShowCanvas", RpcTarget.AllBuffered);
-    //        }
-
     //        // 캐릭터는 동작, 회전을 멈추고
 
+    //        // 현재 캔버스를 true로
+    //        UI_Manager.instance.OnCoffee();
     //    }
     //}
 
 
-    //[PunRPC]
-    //void ShowCanvas()
-    //{
-    //    // 현재 캔버스를 true로
-    //    UI_Manager.instance.OnCoffee();
-    //}
+    ////////////////////// 포톤 멀티 플레이용 
+    void OnTriggerEnter(Collider other)
+    {
+        // player move 컴포넌트를 가진 other 가 충돌하면
+        if (other.CompareTag("Player"))
+        {
+            PhotonView photonview = other.GetComponent<PhotonView>();
+
+            if (photonview != null && photonview.IsMine)
+            {
+                photonview.RPC("ShowCanvas", RpcTarget.AllBuffered);
+            }
+
+            // 캐릭터는 동작, 회전을 멈추고
+
+        }
+    }
+
+
+    [PunRPC]
+    void ShowCanvas()
+    {
+        // 현재 캔버스를 true로
+        UI_Manager.instance.OnCoffee();
+    }
 }
--- Assets/PJH/P_Scripts/Legal_Trigger.cs
+++ Assets/PJH/P_Scripts/Legal_Trigger.cs
@@ -7,45 +7,45 @@ public class Legal_Trigger : MonoBehaviour
 {
     // 싱글
 
-    void OnTriggerEnter(Collider other)
-    {
-        // player move 컴포넌트를 가진 other 가 충돌하면
-        if (other.gameObject.CompareTag("Player"))
-        {
-            // 캐릭터는 동작, 회전을 멈추고
-
-            // 현재 캔버스를 true로
-            UI_Manager.instance.OnLegalPad();
-        }
-    }
-
-
-    ////////////////// 포톤 멀티플레이 적용
     //void OnTriggerEnter(Collider other)
     //{
     //    // player move 컴포넌트를 가진 other 가 충돌하면
-    //    if (other.CompareTag("Player"))
+    //    if (other.gameObject.CompareTag("Player"))
     //    {
-    //        {
-    //            // 충돌한 플레이어의 photonview 컴포넌트를 가져온다.
-    //            PhotonView photonview = other.GetComponent<PhotonView>();
-
-    //            if(photonview != null && photonview.IsMine)
-    //            {
-    //                photonview.RPC("ShowCanvas", RpcTarget.AllBuffered);
-    //            }
-
-    //        }
     //        // 캐릭터는 동작, 회전을 멈추고
 
-
+    //        // 현재 캔버스를 true로
+    //        UI_Manager.instance.OnLegalPad();
     //    }
     //}
 
-    //[PunRPC]
-    //void ShowCanvas()
-    //{
-    //    // ui매니저 스크립트에서 캔버스 함수를 실행한다.
-    //    UI_Manager.instance.OnLegalPad();
-    //}
+
+    //////////////// 포톤 멀티플레이 적용
+    void OnTriggerEnter(Collider other)
+    {
+        // player move 컴포넌트를 가진 other 가 충돌하면
+        if (other.CompareTag("Player"))
+        {
+            {
+                // 충돌한 플레이어의 photonview 컴포넌트를 가져온다.
+                PhotonView photonview = other.GetComponent<PhotonView>();
+
+                if (photonview != null && photonview.IsMine)
+                {
+                    photonview.RPC("ShowCanvas", RpcTarget.AllBuffered);
+                }
+
+            }
+            // 캐릭터는 동작, 회전을 멈추고
+
+
+        }
+    }
+
+    [PunRPC]
+    void ShowCanvas()
+    {
+        // ui매니저 스크립트에서 캔버스 함수를 실행한다.
+        UI_Manager.instance.OnLegalPad();
+    }
 }
